e47fc30f4996701182feee52c89a4ff2ed7b7c92
Ashmit Singh <ashmitsingh361@gmail.com>
2026-03-17 14:59:56 +0530

    readys

--- client/my-vyapar/app/page.tsx
+++ client/my-vyapar/app/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Navbar from "../components/Navbar"; 
 import Hero from "../components/Hero";
 import Features from "../components/Features"; 
@@ -11,6 +12,17 @@ import Ratings from "../components/Ratings";
 
 
 import Footer from "../components/Footer";
+=======
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+// import Features from "./components/Features";
+import Industries from "../components/Industries";
+import Pricing from "../components/Pricing";
+import Testimonials from "../components/Testimonials";
+import FAQ from "../components/FAQ";
+import Ratings from "../components/Ratings";
+// import Footer from "../components/Footer";
+>>>>>>> 9bfd81b (my second git push)
 
 export default function Page() {
   return (
@@ -25,9 +37,17 @@ export default function Page() {
        <Start />
       
       <FAQ />
+<<<<<<< HEAD
      <Ratings />
       <Footer/>
       </main>
 
   )
   };
+=======
+      <Ratings />
+      {/* <Footer /> */}
+    </>
+  );
+}
+>>>>>>> 9bfd81b (my second git push)
