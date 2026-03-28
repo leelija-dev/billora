3fdc4dded2492f88ada972ff8ac8f4c8ef2656c5
Ashmit Singh <ashmitsingh361@gmail.com>
2026-03-28 10:27:58 +0530

    modify config

--- client/my-vyapar/next.config.js
+++ client/my-vyapar/next.config.js
@@ -1,7 +1,23 @@
 const nextConfig = {
   devIndicators: false,
   turbopack: {
-    root: process.cwd(), // This sets the root directory for turbopack
+    root: process.cwd(),
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.dummyjson.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        port: '',
+        pathname: '/img/**',
+      },
+    ],
   },
 };
 
