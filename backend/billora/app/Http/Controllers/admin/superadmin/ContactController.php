2edf50aeadb9ca68ed4eff45cca5700ff2613ee1
Lakshman Pal <lakshman.leelija@gmail.com>
2026-04-13 12:37:15 +0530

    update contact us

--- backend/billora/app/Http/Controllers/admin/superadmin/ContactController.php
+++ backend/billora/app/Http/Controllers/admin/superadmin/ContactController.php
@@ -7,10 +7,29 @@ use Illuminate\Http\Request;
 use App\Models\ContactUs;
 class ContactController extends Controller
 {
-    public function index(){
-        $contacts = ContactUs::orderBy('created_at', 'desc')->paginate(10);
-        return view('admin.contact_us.index', compact('contacts'));
-    }
+    public function index(Request $request)
+{
+    $search = $request->input('search');
+
+    $contacts = ContactUs::query()
+        ->when($search, function ($query, $search) {
+            if (strtolower($search) === 'new') {
+                $query->where('view_status', 0);
+            } else {
+            $query->where(function ($q) use ($search) {
+                $q->where('name', 'like', "%{$search}%")
+                  ->orWhere('email', 'like', "%{$search}%")
+                  ->orWhere('subject', 'like', "%{$search}%")
+                  ->orWhere('message', 'like', "%{$search}%")
+                  ->orWhere('created_at', 'like', "%{$search}%");
+            });
+            }
+        })
+        ->orderBy('created_at', 'desc')
+        ->paginate(10);
+
+    return view('admin.contact_us.index', compact('contacts'));
+}
     public function view($id){
         $contacts = ContactUs::findOrFail($id);
         $contacts->view_status = true;
--- backend/billora/resources/views/admin/contact_us/index.blade.php
+++ backend/billora/resources/views/admin/contact_us/index.blade.php
@@ -600,7 +600,7 @@
 
 
                             </button>
-                            <form action="{{ route('admin.customers.index') }}" method="GET">
+                            {{-- <form action="{{ route('admin.customers.index') }}" method="GET">
 
                                 <select name="status" onchange="this.form.submit()"
                                     class="px-3 py-2 border rounded text-sm">
@@ -617,7 +617,7 @@
 
                                 </select>
 
-                            </form>
+                            </form> --}}
                         </div>
 
                         <!-- Right: Search -->
@@ -626,8 +626,8 @@
                                 <path
                                     d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zM9.5 14C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
                             </svg>
-                            <form action="{{ route('admin.customers.index') }}" method="GET">
-                                <input type="text" name="search" placeholder="Search customers..."
+                            <form action="{{ route('admin.contacts.index') }}" method="GET">
+                                <input type="text" name="search" placeholder="Search contacts..."
                                     value="{{ request('search') }}" {{-- id="searchInput" --}}
                                     class="outline-none px-8 py-3 text-sm">
                             </form>
@@ -820,18 +820,18 @@
 
             });
 
-            /* =========================
-               CLEAR SELECTION
-            ========================= */
+            
+            //    CLEAR SELECTION
+           
             function clearSelection() {
                 localStorage.removeItem('selectedCustomers');
                 localStorage.removeItem('isAllSelected');
                 location.reload();
             }
 
-            /* =========================
-               SEND MAIL
-            ========================= */
+         
+            //    SEND MAIL
+           
             function handleSendMail() {
 
                 let selected = JSON.parse(localStorage.getItem('selectedCustomers')) || [];
@@ -846,7 +846,7 @@
                     return;
                 }
 
-                let url = "{{ route('admin.customers.customer-mail') }}";
+                let url = "#";
 
                 //  GET search from URL (BEST METHOD)
                 let params = new URLSearchParams(window.location.search);
