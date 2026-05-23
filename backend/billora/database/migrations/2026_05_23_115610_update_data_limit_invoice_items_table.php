05c766fffb8d308f8329c8fd5a2246e9820447c4
Lakshman Pal <lakshman.leelija@gmail.com>
2026-05-23 12:05:53 +0530

    update invoice items table price limit

---
+++ backend/billora/database/migrations/2026_05_23_115610_update_data_limit_invoice_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('invoice_items', function (Blueprint $table) {
+            $table->decimal('price', 12, 2)->default(0)->change();
+            $table->decimal('total_price', 12, 2)->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('invoice_items', function (Blueprint $table) {
+            $table->decimal('price', 10, 2)->default(0)->change();
+            $table->decimal('total_price', 10, 2)->default(0)->change();
+        });
+    }
+};
